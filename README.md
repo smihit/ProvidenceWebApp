bb0deb71fd8bb67ef25cfa23060da65925c0ddb8
Smihit Shah <smsha@microsoft.com>
2018-08-11 20:17:47 -0700

    Index html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ProvidenceWebApp
+
+C:\Users\smsha\Downloads>ffmpeg -i star_trails.mp4 -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls star_trails_hls.m3u8
--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route('/video_feed')
+def video_feed():
+    return send_file(r'C:\Users\smsha\Downloads\star_trails_hls.m3u8', attachment_filename="video.m3u8")
 
 
 if __name__ == "__main__":
